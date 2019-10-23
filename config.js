707c3824015d2b2df625465e59bc92f3c140afcc
Valerio Nassi <v.nassi@reply.it>
2019-10-23 12:22:19 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC9S9r6xW6jXyrd5Pqr9TSldktExvY3SMM",
+  authDomain: "chatapp-aesys.firebaseapp.com",
+  databaseURL: "https://chatapp-aesys.firebaseio.com",
+  projectId: "chatapp-aesys",
+  storageBucket: "chatapp-aesys.appspot.com",
+  messagingSenderId: "292709897054",
+  appId: "1:292709897054:web:e0d6a762e89aca018735cf",
+  measurementId: "G-S1M31L9YSH"
+};
+
+export default firebaseConfig;
